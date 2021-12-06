4df67b647f50f25960ac9d8a65ae638a4f48a48b
mahiedinne-bouchouareb <91871267+mahiedinne-bouchouareb@users.noreply.github.com>
2021-12-06 12:04:16 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
 
-$db = mysqli_connect("localhost","root", "", "moduleconnexion");
+$db = mysqli_connect("localhost:3306","mahiedinne", "12345", "mahiedinne-bouchouareb_moduleconnexion");
 $requete =mysqli_query ($db ,"SELECT * FROM  utilisateurs ");
 $query = mysqli_fetch_all($requete,MYSQLI_ASSOC);
 
